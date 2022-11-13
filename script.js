47eddd6abd0fc3859817e4c1a08a745cf68fcbff
Jordan Latter <Jlatter1111@gmail.com>
2022-11-13 20:20:56 +0000

    add clear and delete buttons and add styling to the page

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const numberBtns= document.querySelectorAll(`[number]`)
 let display = document.getElementById("result")
+let clear = document.getElementById("clear")
+let del = document.getElementById("delete")
 let currentNumber =""
 let secondNumber = ""
 let realSecond =  parseFloat(secondNumber)
 display.innerHTML=0
 let i=0
 let result =0
+let number
 numberBtns.forEach((button)=> button.addEventListener("click", event=>displayNum(button.textContent)))
 
 const displayNum=function(number){
+  
   console.log(number +" this is the first input")
   if(display.innerHTML==="0"){display.innerHTML=""};
   display.innerHTML+= number;
   if(number==="0"||number==="1"||number==="2"||number==="3"||number==="4"||number==="5"||number==="6"||number==="7"||number==="8"||number==="9"||number==="."){currentNumber+= (number);}
+  
   console.log("this is current num before changes made " +currentNumber)
     if(i===1 && number==="+"||i===1 &&number==="-"||i===1 &&number==="x"||i===1 &&number==="÷"||i===1 &&number==="="){add(secondNumber,currentNumber);console.log("second num "+secondNumber+"current num "+currentNumber)}
     else if(i===2 && number==="+"||i===2 &&number==="-"||i===2 &&number==="x"||i===2 &&number==="÷"||i===2 &&number==="="){subtract(secondNumber,currentNumber)}
@@ -25,6 +30,19 @@ const displayNum=function(number){
   else if (number=== "="){;display.innerHTML = currentNumber; number=currentNumber};
    
 };
+clear.addEventListener("click", event=>clearing())
+function clearing(){
+  currentNumber =""
+  secondNumber = ""
+  display.innerHTML = 0
+  i= 0
+  result=0
+}
+del.addEventListener("click", event=>deletes(number))
+function deletes(number){
+  display.innerHTML =display.innerHTML.slice(0,-1)
+  currentNumber = currentNumber.slice(0,-1)
+}
 
 function numSwitch(){
   secondNumber=currentNumber;
